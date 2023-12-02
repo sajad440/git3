41577380eb27a394694147c9f5e1fc2392849a2a
sajad <sajad.akhlaghi004@gmail.com>
2023-12-02 10:17:20 +0330

    power

---
+++ ppower.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace git3
+{
+    class ppower
+    
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("enter number :");
+            var number = Convert.ToInt32(Console.ReadLine());
+             System.Console.WriteLine("enter power :");
+            var power = Convert.ToInt32(Console.ReadLine());
+            var result =P0ower(number , power);
+            System.Console.WriteLine(result);
+
+
+
+        }
+
+        static int P0ower(int number , int power )
+        {
+            int result = 1 ; 
+            for (int i = 0  ; i < power ; i++)
+            {
+                result*=number ;
+           
+            }
+          return result ;
+        }
+    }
+}
